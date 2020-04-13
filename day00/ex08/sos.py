cc641d917c1f090cc3bb99d4d95d9b9ced50d1b5
dochoi <ehdrb345@naver.com>
2020-04-14 04:51:03 +0900

    good

---
+++ day00/ex08/sos.py
@@ -0,0 +1,30 @@
+
+from string import punctuation
+import sys
+
+dic = {'0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-',
+        '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.',
+        'A':'.-', 'B':'-...', 'C':'-.-.', 'D':'-..', 'E':'.', 'F':'..-.',
+        'G':'--.', 'H':'....', 'I':'..', 'J':'.---', 'K':'-.-', 'L':'.-..',
+        'M':'--', 'N':'-.', 'O':'---', 'P':".--.",'R':'.-.', 'S':'...', 'T':'-', 'U':'..-',
+        'V':'...-', 'W':'.--', 'X':'-..-', 'Y':'-.--', 'Y':'-.--', 'Z':'--..',
+        " ": "/"}
+
+if __name__ == "__main__":
+    i = 1
+    len_argv = len(sys.argv)
+    if len_argv == 1:
+        print("ERROR")
+        sys.exit()
+    else:
+        while i < len_argv:
+            s = sys.argv[i].replace(" ", "")
+            if not s.isalnum():
+                print("ERROR")
+                sys.exit()
+            i += 1
+    text = " ".join(sys.argv[1::])
+    text = text.upper()
+    for c in text:
+        print(dic[c], end=' ')
+    print("")
---
+++ day00/ex09/guess.py
@@ -0,0 +1,37 @@
+
+import random
+import sys
+
+if __name__ == "__main__":
+    answer = random.randint(1,99)
+    n = 0
+    cnt = 0
+
+    print("This is an interactive guessing game!")
+    print("You have to enter a number between 1 and 99 to find out the secret number.")
+    print("Type 'exit' to end the game.\nGood luck!\n")
+    while True:
+        cnt += 1
+        print("What's your guess between 1 and 99?")
+        n = input()
+        if n == 'exit':
+            print("Goodbye!")
+            break
+        if not n.isdecimal():
+            print("That's not a number.")
+            continue
+        n_int = int(n)
+        if (n_int < 1 or n_int > 99):
+            print("The number must be between 1 and 10.")
+        elif answer > n_int:
+            print("Too low!")
+        elif answer < n_int:
+            print("Too high!")
+        elif answer == n_int:
+            if n_int == 42:
+                print("The answer to the ultimate question of life, the universe and everything is 42.")
+            if cnt == 1:
+                print("Congratulations! You got it on your first try!")
+            else :
+                print("Congratulations, you've got it! You won in",cnt ,"attempts!")
+            break
