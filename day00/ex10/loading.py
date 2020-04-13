901513425224aa19f5a3c6928b4366f96b08e329
dochoi <ehdrb345@naver.com>
2020-04-14 06:03:23 +0900

    fin

---
+++ day00/ex10/loading.py
@@ -0,0 +1,23 @@
+
+from time import time, sleep
+import sys
+
+def ft_progress(lst):
+    start = time()
+    maximum = len(lst)
+    for elem in lst:
+        yield elem
+        cur = time() - start
+        elem += 1
+        per = (elem / maximum * 100.0)
+        eta = ((maximum * cur) / elem)
+        bar = "=" * int(elem / maximum * 24) + ">"
+        print("\rETA: %.2fs [%3.0f%%][%-24s] %d/%d | elapsed time %.2fs" % (eta, per, bar, elem,maximum, cur),end = '')
+if __name__ == "__main__":
+    listy = range(1000)
+    ret = 0
+    for elem in ft_progress(listy):
+        ret += (elem + 3) % 5
+        sleep(0.01)
+    print()
+    print(ret)
